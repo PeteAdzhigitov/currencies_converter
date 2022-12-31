5add0ff3dbfcbb83060f831afceaa3f6114c6e65
Petr <adzhigitovpetr@gmail.com>
2022-12-31 03:07:22 +0300

    Rewrite main_page.html and core.views.py to use forms.py instead of html tags

--- currencies_converter/core/forms.py
+++ currencies_converter/core/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 
+currencies = {
+'AUD':48,
+'AZN':41,
+'AMD':18,
+'BYN':26,
+'BGN':38,
+'BRL':13,}
 
+c = [(k,k) for k,v in currencies.items()]
 
 class QuantyToConvert(forms.Form):
-    quantity_of_money_to_convert = forms.IntegerField(min_value=1)
-    convert_from = forms.ChoiceField()
-    convert_to = forms.ChoiceField()
+    quantity_of_money_to_convert = forms.IntegerField(min_value=1,)
+    convert_from = forms.ChoiceField(choices=c)
+    convert_to = forms.ChoiceField(choices=c)
+
 
--- currencies_converter/core/views.py
+++ currencies_converter/core/views.py
@@ -2,25 +2,29 @@ import csv
 import requests
 
 from django.shortcuts import render
-# from currencies_converter.core.forms import QuantyToConvert
+from .forms import QuantyToConvert
 
 # Create your views here.
 
+
+
 def currencies_logic(request):
-    currencies = {'AUD':'48,0809',
-'AZN':'41,9565',
-'AMD':'18,1367',
-'BYN':'26,3274',
-'BGN':'38,7368',
-'BRL':'13,5013',}
+    currencies = {
+        'AUD':48,
+        'AZN':41,
+        'AMD':18,
+        'BYN':26,
+        'BGN':38,
+        'BRL':13,}
     # API_KEY = 'ptN804DgBqvzwVvtHPnDHwoMTSrq0lAD'
     # get_data = request('https://api.exchangeratesapi.io/v1/latest?access_key=API_KEY')
 
     # form = QuantyToConvert(request.POST)
 
     if request.method == "GET":
-        with open('currencies.csv','r',encoding='UTF-8') as file:
-            csvreader = csv.reader(file, delimiter='|')
+        form = QuantyToConvert()
+        # with open('currencies.csv','r',encoding='UTF-8') as file:
+        #     csvreader = csv.reader(file, delimiter='|')
             # d = []
             # for row in csvreader:
             #     d.append(row)
@@ -32,15 +36,18 @@ def currencies_logic(request):
                 #         convert_to = form.cleaned_data['convert_to']
 
                         # result = (quantity_of_money * convert_from) / convert_to
-            return render(request, 'core/main_page.html', {'csvreader': currencies})
+        return render(request, 'core/main_page.html', {'form':form,'csvreader':currencies})
     if request.method == 'POST':
-        data = request.POST
-        curr_from = data.get('convert_from')
-        curr_to = data.get('convert_to')
-        amount_of_money = data.get('quantity_of_money_to_convert')
-        converted_sum = float((amount_of_money*curr_from)/curr_to)
+        form = QuantyToConvert(request.POST)
+        if form.is_valid():
+            curr_from = form.cleaned_data.get('convert_from')
+            curr_to = form.cleaned_data.get('convert_to')
+            amount_of_money = form.cleaned_data.get('quantity_of_money_to_convert')
+            converted_sum = round((currencies[curr_from]/(currencies[curr_to]))*float(amount_of_money),2)
+            # converted_sum = curr_to
+            return render(request,'core/main_page.html',{'converted_sum':converted_sum,'form':form})
 
-        return render(request,'core/main_page.html',{'converted_sum':converted_sum,'curr_from':curr_from,'curr_to':curr_to, 'amount_of_money':amount_of_money })
+        return render(request,{'form':form})
 
 
 

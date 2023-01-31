21580f4702d174981235741c3d3a2376af87129d
petradzigitov <adzhigitovpetr@gmail.com>
2023-01-31 23:24:51 +0500

    Added a couple of tests

--- currencies_converter/core/tests.py
+++ currencies_converter/core/tests.py
@@ -4,6 +4,15 @@ from django.test import TestCase
 from requests import request
 
 class Test:
-    def testitng_purposes(self):
+    # def testitng_purposes(self):
+    #
+    #     get_data = request('https://api.exchangeratesapi.io/v1/latest?access_key=API_KEY')
 
-        get_data = request('https://api.exchangeratesapi.io/v1/latest?access_key=API_KEY')
+    def testing_that_true_is_true(self):
+        assert True == True
+
+    def testing_sum_of_two_numbers(self):
+        a = 1
+        b = 2
+        result = a + b
+        assert result == 3
